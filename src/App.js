e0f6ee5eeefa86091a6513a1ace988450254f2d9
boreumHome <choboreum@naver.com>
2022-08-12 20:12:34 +0900

    app.js 준비

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import {Button, Navbar, Container, Nav, NavDropdown, Row, Col} from 'react-bootstrap'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 export default class App extends Component {
   state = {
     id : "",
@@ -26,12 +28,30 @@ export default class App extends Component {
   };
 
   render() {
-    return (
-      <div>
-        <input onChange={this.handleChange} name ="id"/>
-        <button onClick = {this.submitId}>Submit</button>
-        <h1>{this.state.id}</h1>
-      </div>
-    )
+    return (<>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </>)
   }
 }
