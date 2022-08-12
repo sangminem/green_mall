86fad124fbd61cc340e7707c46eeb712cb6a4de9
chy <chy@gmail.com>
2022-08-12 17:56:43 +0900

    first test

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,11 +1,24 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Category = () => {
+
+    const [product, setProduct] = useState([{id:0, title:"first", desc:"desc1"}, {id:1, title: "second", desc:"desc2"}]);
+
     return (
         <React.Fragment>
-            카테고리별 리스트 페이지
+            {
+                product.map((a, i) => {
+                    return (
+                        <React.Fragment key={i}>
+                            <h2>{a.title}</h2>
+                            <span>{a.desc}</span>
+                        </React.Fragment>   
+                    )
+                })
+            }
         </React.Fragment>
     );
   }
-  
+
+
   export default Category;
