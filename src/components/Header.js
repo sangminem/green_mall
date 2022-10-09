f6bd2d1dac6cddd3a9b1bdc537766c0d1350e58a
velhy <hayeon.it@gmail.com>
2022-10-09 16:53:02 +0900

    회원가입 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,47 @@
-import React, { Component } from 'react'
-import {Button, Navbar, Container, Nav, NavDropdown, Row, Col} from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter, Route, Routes, useHistory} from "react-router-dom";
+import React, { Component } from "react";
+import {
+  Navbar,
+  Container,
+  Nav,  
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes, useHistory } from "react-router-dom";
 import { IoCartOutline } from "react-icons/io5";
+import { Tabs } from "antd";
+import CategoryPage from "../pages/CategoryPage";
 
 const Header = () => {
-    return (<>       
-        <Navbar bg="light" variant="light">
-            <Container>
-            <Navbar.Brand as={Link} to="/">GREEN MALL</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to="/category/furniture">가구</Nav.Link>
-                <Nav.Link as={Link} to="/category/plant">식물/데코</Nav.Link>
-                <Nav.Link as={Link} to="/sale">SALE</Nav.Link>
-                <Nav.Link as={Link} to="/productMng">상품관리</Nav.Link>
-                <Nav.Link as={Link} to="/"><IoCartOutline/></Nav.Link>
-            </Nav>
-            </Container>
-        </Navbar>
-    </>)
-}
+  return (
+    <>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            GREEN MALL
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/category/furniture">
+              가구
+            </Nav.Link>
+            <Nav.Link as={Link} to="/category/plant">
+              식물/데코
+            </Nav.Link>
+            <Nav.Link as={Link} to="/sale">
+              SALE
+            </Nav.Link>
+            <Nav.Link as={Link} to="/productMng">
+              상품관리
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              <IoCartOutline />
+            </Nav.Link>
+          </Nav>       
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
 export default Header;
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,11 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment } from "react";
+import { Container } from "react-bootstrap";
+import { Button, Input, Form, Modal } from "antd";
 
 const SignupPage = () => {
-    return (
-        <React.Fragment>
-            회원가입 페이지
-        </React.Fragment>
-    );
-  }
-  
-  export default SignupPage;
+  return (
+    <React.Fragment>
+      <Container>
+        <p className="tit-lg">회원가입</p>
+        <Form labelCol={{ span: 4 }} wrapperCol={{ span: 18 }}>
+          <Form.Item label="이메일">
+            <Input name="email_id" placeholder="이메일을 입력하세요" />
+          </Form.Item>
+          <Form.Item label="이름">
+            <Input name="user_nm" placeholder="이름을 입력하세요"/>
+          </Form.Item>
+          <Form.Item label="비밀번호">
+            <Input name="password" placeholder="비밀번호를 입력하세요"/>
+          </Form.Item>
+          <Button type="primary">회원가입</Button>
+        </Form>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default SignupPage;
